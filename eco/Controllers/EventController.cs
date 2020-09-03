fb3ba97710ecb56265612cf25daf003320ce1541
debbieccc <yitung532@hotmail.com>
2020-09-03 11:45:06 +0800

    動態欄位

--- eco/Controllers/EventController.cs
+++ eco/Controllers/EventController.cs
@@ -18,23 +18,91 @@ namespace eco.Controllers
             return View(events);
         }
 
-        public ActionResult Event2()
+        public ActionResult Event_page(int? id)
         {
-            IQueryable<tEventSet> events = null;
+            if (id == null)
+            {
+                return RedirectToAction("Event");
+            }
             TestForEventRegisterEntities db = new TestForEventRegisterEntities();
-            events = from t in (new TestForEventRegisterEntities()).tEventSet
-                        select t;
-            return View(events);
+            tEventSet x = db.tEventSet.FirstOrDefault(m => m.fEventId == id);
+            return View(x);
         }
 
-        public ActionResult Event_yoga()
-        {
-            return View();
-        }
 
-        public ActionResult Event_signup()
+        public ActionResult Event_signup(int? id)
+        {            
+            TestForEventRegisterEntities db = new TestForEventRegisterEntities();
+            if (id == null)
+            {
+                return RedirectToAction("Event");
+            }
+            tEventSet set = db.tEventSet.FirstOrDefault(m => m.fEventId == id);
+            tEventRegister x = new tEventRegister();           
+            x.fEventId = id;
+
+            if (set.ferEmail == 1)
+            {
+                ViewBag.ferEmail = 1;
+            }
+            if (set.ferGender == 1)
+            {
+                ViewBag.ferGender = 1;
+            }
+            if (set.ferBirthday == 1)
+            {
+                ViewBag.ferBirthday = 1;
+            }
+            if (set.ferIdentity == 1)
+            {
+                ViewBag.ferIdentity = 1;
+            }
+            if (set.ferOccupation == 1)
+            {
+                ViewBag.ferOccupation = 1;
+            }
+            if (set.ferVeganOrNot == 1)
+            {
+                ViewBag.ferVeganOrNot = 1;
+            }
+            if (set.ferGender == 1)
+            {
+                ViewBag.ferGender = 1;
+            }
+            if (set.ferOtherColumn1 !=null)
+            {
+                ViewBag.ferOtherColumn1 = set.ferOtherColumn1;
+            }
+            if (set.ferOtherColumn2 != null)
+            {
+                ViewBag.ferOtherColumn2 = set.ferOtherColumn2;
+            }
+            if (set.ferOtherColumn3 != null)
+            {
+                ViewBag.ferOtherColumn3 = set.ferOtherColumn3;
+            }
+            if (set.ferOtherColumn4 != null)
+            {
+                ViewBag.ferOtherColumn4 = set.ferOtherColumn4;
+            }
+            if (set.ferOtherColumn5 != null)
+            {
+                ViewBag.ferOtherColumn5 = set.ferOtherColumn5;
+            }
+
+
+            return View(x);
+        }     
+
+        [HttpPost]
+        public ActionResult Event_signup(tEventRegister p)
         {
-            return View();
+            TestForEventRegisterEntities db = new TestForEventRegisterEntities();     
+            
+            db.tEventRegister.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Event");
+
         }
     }
 }
---
+++ eco/JS/eventsignup.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    
+    if ($("#ferEmail").attr("showornot") == 1) {
+        $("#ferEmail").css("display", "block")
+    }
+    if ($("#ferGender").attr("showornot") == 1) {
+        $("#ferGender").css("display", "block")
+    }
+    if ($("#ferBirthday").attr("showornot") == 1) {
+        $("#ferBirthday").css("display", "block")
+    }
+    if ($("#ferIdentity").attr("showornot") == 1) {
+        $("#ferIdentity").css("display", "block")
+    }
+    if ($("#ferOccupation").attr("showornot") == 1) {
+        $("#ferOccupation").css("display", "block")
+    }
+    if ($("#ferVeganOrNot").attr("showornot") == 1) {
+        $("#ferVeganOrNot").css("display", "block")
+    }
+    if ($("#ferOtherColumn1").attr("showornot") != 0) {
+        $("#ferOtherColumn1").css("display", "block")
+    }
+    if ($("#ferOtherColumn2").attr("showornot") != 0) {
+        $("#ferOtherColumn2").css("display", "block")
+    }
+    if ($("#ferOtherColumn3").attr("showornot") != 0) {
+        $("#ferOtherColumn3").css("display", "block")
+    }
+    if ($("#ferOtherColumn4").attr("showornot") != 0) {
+        $("#ferOtherColumn4").css("display", "block")
+    }
+    if ($("#ferOtherColumn5").attr("showornot") != 0) {        
+        $("#ferOtherColumn5").css("display", "block")
+    }
+    });
